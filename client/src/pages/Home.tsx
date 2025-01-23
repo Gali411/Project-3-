0a00e31a64fcfd6c23f6021de9bca22f37d1f407
Gabriel Harris <gabriel11harris030@MacBookAir.lan>
2025-01-22 20:07:05 -0500

    making a link show up in browser

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-    return (
-      <div>
-        <h1>Can you see me?dddddd</h1>
-      </div>
-    );
-  }
-  
+ 
+  const [link, setLink] = useState(null);
+
+  useEffect(() => {
+    
+    fetch("https://api.song.link/v1-alpha.1/links?url=https%3A%2F%2Fopen.spotify.com%2Ftrack%2F4pnupHcsf2U60BY1SzMwJZ&userCountry=US&songIfSingle=true")
+      .then(response => response.json())
+      .then(data => {
+        
+        const amazonMusicLink = data.linksByPlatform.amazonMusic?.url;
+        setLink(amazonMusicLink);  
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>
+        {link ? (
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            Amazon Music Link
+          </a>
+        ) : (
+          'Loading link...'
+        )}
+      </h1>
+    </div>
+  );
+}
